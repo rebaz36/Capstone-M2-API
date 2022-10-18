1c0b5fbd6a50d16303a2a18d411bc9225e4710f0
rebaz36 <rebaz415@gmail.com>
2022-10-19 01:15:10 +0300

    add display movies function to indexjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import _ from 'lodash';
-import displayHero from './modules/displayHero';
+import { displayHero, Displaymovies } from './modules/displayHero.js';
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', displayHero(0, 'image'));
+document.addEventListener('DOMContentLoaded', Displaymovies(20));
