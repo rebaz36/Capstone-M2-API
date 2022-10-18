f9e7e551fcdedc1c0501e1e689fb8ae3760979e5
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-18 13:08:06 -0500

    Create function to get data from Ghibli API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import _ from 'lodash';
 import './styles.css';
+
+const getData = (number, section) => {
+  fetch('https://ghibliapi.herokuapp.com/films')
+    .then((response) => response.json())
+    .then((data) => {
+      const imageContainer = document.querySelector('.hero-image');
+      imageContainer.innerHTML += `<img src="${data[number][section]}" />`;
+      console.log(data);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', getData(0, 'image'));
