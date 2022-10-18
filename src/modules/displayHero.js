6a8a51cbcc280f4a0bc35fecfa785c705e7664d2
rebaz36 <rebaz415@gmail.com>
2022-10-19 01:14:50 +0300

    Add Display movie function

--- src/modules/displayHero.js
+++ src/modules/displayHero.js
@@ -1,4 +1,4 @@
-const displayHero = (number, section) => {
+export const displayHero = (number, section) => {
   fetch('https://ghibliapi.herokuapp.com/films')
     .then((response) => response.json())
     .then((data) => {
@@ -7,4 +7,26 @@ const displayHero = (number, section) => {
     });
 };
 
-export default displayHero;
+export const Displaymovies = (number) => {
+  fetch('https://ghibliapi.herokuapp.com/films')
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < number; i += 1) {
+        const hour = Math.floor((data[i].running_time / 60));
+        const minutes = (data[i].running_time % 60);
+        const listContainer = document.querySelector('.list-container');
+        listContainer.innerHTML += `<div class="movie-container">
+          <img class="movie-image" src="${data[i].image}" alt="${data[i].title}">
+            <div class="movie-info">
+              <h3>${data[i].title}</h3>
+              <p>${data[i].release_date}</p>
+              <p>${hour}h ${minutes} min</p>
+            </div>
+            <div class="movie-buttons">
+              <button class="like-button">Like</button>
+              <button class="comment-button">Comment</button>
+            </div>
+          </div>`;
+      }
+    });
+};
