8f6bd81da4cad4fa1916e46f5985cb54b2b02be9
rebaz36 <rebaz415@gmail.com>
2022-10-19 22:55:17 +0300

    update displayModal Function

--- src/modules/displayModal.js
+++ src/modules/displayModal.js
@@ -1,41 +1,41 @@
+import ghibilapi from './API/ghibliApi.js';
+import { modalBg, modalContainer, body } from './elements/elements.js';
 /* displayModal.js */
-const modalBg = document.querySelector('.modal-bg');
-const modalContainer = document.querySelector('.modal-content');
 
 export const hideModal = () => {
   modalBg.style.display = 'none';
   modalContainer.style.display = 'none';
+  body.style.overflow = 'auto';
 };
 
-const displayModal = async (number) => {
+export async function displayModal(number) {
   modalBg.style.display = 'flex';
   modalContainer.style.display = 'flex';
-  await fetch('https://ghibliapi.herokuapp.com/films')
-    .then((response) => response.json())
-    .then((data) => {
-      modalContainer.innerHTML = `
-      <i class="fa-solid fa-xmark fa-2xl"></i>
-      <img src="${data[number].image}"/>
-      <div class="information">
-        <div class="left">
-        <p class="indicator">Title: <span>${data[number].title}</span></p>
-        <p class="indicator">Original Title: <span>${data[number].original_title}</span></p>
-        </div>
-        <div class="right">
-        <p class="indicator">Release date: <span>${data[number].release_date}</span></p>
-        <p class="indicator">Director: <span>${data[number].director}</span></p>
-        
-        </div>
-      </div>
-      <p class="description indicator">Description: <span>${data[number].description}</span></p>
-      `;
-    });
+  body.style.overflow = 'hidden';
+  const data = await ghibilapi.fetchmovie(number);
+  modalContainer.innerHTML = `
+  <i class="fa-solid fa-xmark fa-2xl"></i>
+  <img src="${data.image}"/>
+  <div class="information">
+    <div class="left">
+    <p class="indicator">Title: <span>${data.title}</span></p>
+    <p class="indicator">Original Title: <span>${data.original_title}</span></p>
+    </div>
+    <div class="right">
+    <p class="indicator">Release date: <span>${data.release_date}</span></p>
+    <p class="indicator">Director: <span>${data.director}</span></p>
+    
+    </div>
+  </div>
+  <p class="description indicator">Description: <span>${data.description}</span></p>
+  `;
+
   modalBg.addEventListener('click', (e) => {
     const { target } = e;
     if (target.classList.contains('modal-bg') || target.classList.contains('fa-xmark')) {
       hideModal();
     }
   });
-};
+}
 
 export default displayModal;
