c63770c3ea15fc071f994a215414a002268bbc46
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-19 11:42:51 -0500

    Fix displayModal bug

--- src/index.js
+++ src/index.js
@@ -9,5 +9,7 @@ document.addEventListener('DOMContentLoaded', displayHero(0, 'image'));
 document.addEventListener('DOMContentLoaded', Displaymovies(20));
 moviesContainer.addEventListener('click', (e) => {
   const { target } = e;
-  displayModal(target.getAttribute('data-index-number'));
+  if (target.matches('.movie-image')) {
+    displayModal(target.getAttribute('data-index-number'));
+  }
 });
