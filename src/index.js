369c5477d38143af1901ac85b56b5aa54913470b
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-19 11:31:26 -0500

    Add event listener to show and hide modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // import _ from 'lodash';
 import { displayHero, Displaymovies } from './modules/displayHero.js';
+import displayModal from './modules/displayModal.js';
 import './styles.css';
+
+const moviesContainer = document.querySelector('.list-container');
 
 document.addEventListener('DOMContentLoaded', displayHero(0, 'image'));
 document.addEventListener('DOMContentLoaded', Displaymovies(20));
+moviesContainer.addEventListener('click', (e) => {
+  const { target } = e;
+  displayModal(target.getAttribute('data-index-number'));
+});
