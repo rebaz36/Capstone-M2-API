43a95d7562f2697007ceecd16c74f203f21a3b98
rebaz36 <rebaz415@gmail.com>
2022-10-19 22:54:32 +0300

    Add File to store Elements

---
+++ src/modules/elements/elements.js
@@ -0,0 +1,10 @@
+// displayModal.js elements
+export const modalBg = document.querySelector('.modal-bg');
+export const modalContainer = document.querySelector('.modal-content');
+export const body = document.querySelector('body');
+
+// displayHero.js elements
+export const imageContainer = document.querySelector('.hero-image');
+
+// displayMovies.js elements
+export const listContainer = document.querySelector('.list-container');
