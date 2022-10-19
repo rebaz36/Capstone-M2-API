75a4f6a49d86996ece8b49613d1a75b405d5f248
rebaz36 <rebaz415@gmail.com>
2022-10-19 22:55:05 +0300

    Update displayMovies Function

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -1,32 +1,23 @@
-export const displayHero = (number, section) => {
-  fetch('https://ghibliapi.herokuapp.com/films')
-    .then((response) => response.json())
-    .then((data) => {
-      const imageContainer = document.querySelector('.hero-image');
-      imageContainer.innerHTML += `<img src="${data[number][section]}"/>`;
-    });
-};
+import { listContainer } from './elements/elements.js';
+import ghibliapi from './API/ghibliApi.js';
 
-export const displayMovies = (number) => {
-  fetch('https://ghibliapi.herokuapp.com/films')
-    .then((response) => response.json())
-    .then((data) => {
-      for (let i = 0; i < number; i += 1) {
-        const hour = Math.floor((data[i].running_time / 60));
-        const minutes = (data[i].running_time % 60);
-        const listContainer = document.querySelector('.list-container');
-        listContainer.innerHTML += `<div class="movie-container">
-          <img class="movie-image" src="${data[i].image}" alt="${data[i].title}" data-index-number=${i}>
-            <div class="movie-info">
-              <h3>${data[i].title}</h3>
-              <p>${data[i].release_date}</p>
-              <p>${hour}h ${minutes} min</p>
-            </div>
-            <div class="movie-buttons">
-              <button class="like-button">Like</button>
-              <button class="comment-button">Comment</button>
-            </div>
-          </div>`;
-      }
-    });
-};
+export default async function displayMovies(number) {
+  const data = await ghibliapi.fetchallmovies();
+
+  for (let i = 0; i < number; i += 1) {
+    const hour = Math.floor((data[i].running_time / 60));
+    const minutes = (data[i].running_time % 60);
+    listContainer.innerHTML += `<div class="movie-container">
+      <img class="movie-image" src="${data[i].image}" alt="${data[i].title}" data-index-number=${i}>
+        <div class="movie-info">
+          <h3>${data[i].title}</h3>
+          <p>${data[i].release_date}</p>
+          <p>${hour}h ${minutes} min</p>
+        </div>
+        <div class="movie-buttons">
+          <button class="like-button">Like</button>
+          <button class="comment-button">Comment</button>
+        </div>
+      </div>`;
+  }
+}
