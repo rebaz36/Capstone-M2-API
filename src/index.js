4e8ee924a083ff4a2ad6327e4d9faa469bbe7706
rebaz36 <rebaz415@gmail.com>
2022-10-19 22:55:45 +0300

    Update dist and Deployment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 // import _ from 'lodash';
-import { displayHero, displayMovies } from './modules/displayMovies.js';
-import displayModal from './modules/displayModal.js';
+import displayHero from './modules/displayHero.js';
+import displayMovies from './modules/displayMovies.js';
+import { displayModal } from './modules/displayModal.js';
 import './styles.css';
 
 const moviesContainer = document.querySelector('.list-container');
+document.onload = displayHero(0);
+document.onload = displayMovies(20);
 
-document.addEventListener('DOMContentLoaded', displayHero(0, 'image'));
-document.addEventListener('DOMContentLoaded', displayMovies(20));
 moviesContainer.addEventListener('click', (e) => {
   const { target } = e;
   if (target.matches('.movie-image')) {
