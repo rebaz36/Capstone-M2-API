2b0d402ecf1841601c1e145ccf1a6f776ff4b238
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-20 13:19:06 -0500

    Add testing for commentsCounter

---
+++ src/modules/tests/commentsCounter.test.js
@@ -0,0 +1,17 @@
+import { commentCounter } from '../ClassCall/classCall';
+
+document.body.innerHTML = '<div class="comments"><h4 id"comments-header"></h4> </div>';
+const commentsListSelector = document.querySelector('.comments');
+
+describe('Comment counter', () => {
+  test('Comment counter should return 0 if the list has no .comment elements', () => {
+    expect(commentCounter.getComments()).toEqual(0);
+  });
+  test('Comment counter should return 1 after adding a comment', () => {
+    const newP = document.createElement('p');
+    newP.classList.add('comment');
+    newP.textContent = 'Hello World!';
+    commentsListSelector.appendChild(newP);
+    expect(commentCounter.getComments()).toEqual(1);
+  });
+});
