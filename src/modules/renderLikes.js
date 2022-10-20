60855db4e6b792c5b49122ad56f169a56177cf56
rebaz36 <rebaz415@gmail.com>
2022-10-21 02:33:39 +0300

    Add function to render the likes

--- src/modules/renderLikes.js
+++ src/modules/renderLikes.js
@@ -1,28 +1,15 @@
 import { involvement } from './ClassCall/classCall.js';
 
-export async function renderLikes() {
-  const movieLikes = await involvement.getLikes();
-  document.querySelectorAll('.like-button').forEach((button, index) => {
-    const likes = 'Likes: ';
-    if (!movieLikes[index]) {
-      button.textContent = `${likes} 0`;
-    } else if (movieLikes[index].likes === 0) {
-      button.textContent = `${likes} 0`;
-    } else {
-      button.textContent = `Likes: ${movieLikes[index].likes}`;
+export default async function renderLikes() {
+  const moviesContainer = document.querySelector('.list-container');
+  moviesContainer.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const { target } = e;
+    const movieId = target.getAttribute('data-index-number');
+    if (target.matches('.like-button')) {
+      const likes = await involvement.getLikes();
+      const likenum = likes.findIndex((item) => item.item_id === movieId);
+      target.innerHTML = `Liked: ${likes[likenum].likes}`;
     }
-    // const numLikes = like[index][likes];
-    // button.innerHTML = `Likes ${like[index][likes]}`;
   });
 }
-
-export async function activeButtons() {
-  document.querySelectorAll('.like-button').forEach((button, index) => {
-    const Index = index;
-    button.addEventListener('click', async () => {
-      await involvement.addLikes(Index);
-    });
-  });
-  renderLikes();
-}
-// const movieLike = Object.values(movieLikes).filter((movie) => movie.item_id === i);
