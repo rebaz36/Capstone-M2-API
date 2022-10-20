620d3a65e2c84c867e5fede13df63bd76e2c2077
rebaz36 <rebaz415@gmail.com>
2022-10-20 14:06:53 +0300

    Initiate renderLikes

--- src/modules/displayMovies.js
+++ src/modules/displayMovies.js
@@ -1,13 +1,16 @@
 import { listContainer } from './elements/elements.js';
 import ghibliapi from './API/ghibliApi.js';
 import { counter } from './ClassCall/classCall.js';
+import { activeButtons, renderLikes } from './renderLikes.js';
 
 export default async function displayMovies(number) {
+  listContainer.innerHTML = '';
   const data = await ghibliapi.fetchallmovies();
 
   for (let i = 0; i < number; i += 1) {
     const hour = Math.floor((data[i].running_time / 60));
     const minutes = (data[i].running_time % 60);
+
     listContainer.innerHTML += `<div class="movie-container">
       <img class="movie-image" src="${data[i].image}" alt="${data[i].title}" data-index-number=${i}>
         <div class="movie-info">
@@ -16,10 +19,13 @@ export default async function displayMovies(number) {
           <p>Duration: ${hour}h ${minutes} min</p>
         </div>
         <div class="movie-buttons">
-          <button class="like-button">Like</button>
+          <button class="like-button">Likes</button>
           <button class="comment-button" data-index-number="${i}">Comment</button>
         </div>
       </div>`;
   }
+  activeButtons();
+  renderLikes();
+
   counter.getItems();
 }
