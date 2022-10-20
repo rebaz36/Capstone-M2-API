b9ba7a5827675da6d883796276745dce0c95a26a
rebaz36 <rebaz415@gmail.com>
2022-10-21 02:34:09 +0300

    Update Main Js File

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import displayHero from './modules/displayHero.js';
 import displayMovies from './modules/displayMovies.js';
 import { displayModal } from './modules/displayModal.js';
 import './styles.css';
+import addLikes from './modules/Likes/addLikes.js';
+import renderLikes from './modules/renderLikes.js';
 
 const moviesContainer = document.querySelector('.list-container');
-document.onload = displayHero(0);
-document.onload = displayMovies(20);
+async function render() {
+  document.onload = await displayHero(0);
+  document.onload = await displayMovies(8);
+  document.onload = await renderLikes();
+  document.onload = await addLikes();
+  moviesContainer.addEventListener('click', (e) => {
+    const { target } = e;
+    if (target.matches('.movie-image') || target.matches('.comment-button')) {
+      displayModal(target.getAttribute('data-index-number'));
+    }
+  });
+}
 
-moviesContainer.addEventListener('click', (e) => {
-  const { target } = e;
-  if (target.matches('.movie-image') || target.matches('.comment-button')) {
-    displayModal(target.getAttribute('data-index-number'));
-  }
-});
+render();
